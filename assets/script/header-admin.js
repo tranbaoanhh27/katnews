d74c3136e93fca04aa5a09429cba6cfd90be04e8
Quoc Trung <buiquoctrung2002@gmail.com>
2023-04-23 16:14:04 +0700

    Thêm các tính năng khi nhấn vào avatar tài khoản cho tất cả các trang

---
+++ assets/script/header-admin.js
@@ -0,0 +1,14 @@
+const userAvatar = document.querySelector('.account-dropdown-toggle');
+const userMenu = document.querySelector('.header__user-menu');
+
+userAvatar.addEventListener('click', function() {
+  userMenu.classList.toggle('header__user-menu--active');
+//   console.log(userMenu);
+});
+
+document.addEventListener('click', function(event) {
+  const isClickInside = userAvatar.contains(event.target) || userMenu.contains(event.target);
+  if (!isClickInside) {
+    userMenu.classList.remove('header__user-menu--active');
+  }
+});
