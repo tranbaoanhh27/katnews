8a37f3009430b0250870ec7631875985608a6dac
phanquocky <phanquocky02@gmail.com>
2023-06-06 17:41:56 +0700

    view template writer

---
+++ controllers/writer/writerController.js
@@ -0,0 +1,30 @@
+const controllers = {}
+
+controllers.showLoginPage = (req, res) => {
+    res.render('writer-login', {layout: 'writer-login-layout'})
+}
+controllers.showRegisterPage = (req, res) => {
+    res.render('writer-register', {layout: 'writer-login-layout'})
+}
+controllers.showOtpPage = (req, res) => {
+    res.render('writer-otp', {layout: 'writer-login-layout'})
+}
+controllers.showForgotPasswordPage = (req, res) => {
+    res.render('writer-forgotPassword', {layout: 'writer-login-layout'})
+}
+controllers.showListNews = (req, res) => {
+    res.render('writer-list-news', {layout: 'writer-news-layout'})
+}
+controllers.showEditPage = (req, res) => {
+    res.render('writer-edit', {layout: 'writer-news-layout'})
+}
+controllers.showInformationPage = (req, res) => {
+    res.render('writer-information', {layout: 'writer-news-layout'})
+}
+controllers.showChangePasswordPage = (req, res) => {
+    res.render('writer-changePassword', {layout: 'writer-news-layout'})
+}
+controllers.showSavedNews = (req, res) => {
+    res.render('writer-savedNews' , {layout: 'writer-news-layout'})
+}
+module.exports = controllers
--- routers/writer/writerRoutes.js
+++ routers/writer/writerRoutes.js
@@ -1,7 +1,16 @@
 var router = require('express').Router();
-
+const controllers = require('../../controllers/writer/writerController')
 router.get('/', (req, res) => {
     res.send("hello it is subdomain of writer");
 })
 
+router.get('/login',  controllers.showLoginPage);
+router.get('/register', controllers.showRegisterPage);
+router.get('/otp', controllers.showOtpPage);
+router.get('/forgotPassword', controllers.showForgotPasswordPage);
+router.get('/listNews', controllers.showListNews);
+router.get('/edit', controllers.showEditPage);
+router.get('/information', controllers.showInformationPage);
+router.get('/changePassword', controllers.showChangePasswordPage);
+router.get('/savedNews', controllers.showSavedNews);
 module.exports = router;
