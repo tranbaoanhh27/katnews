96818da9a0b195b1958370473e000d50f47736c2
tranbaoanhh27 <tranbaoanhh27@gmail.com>
2023-06-10 20:44:27 +0700

    render top 10 most viewed and top 10 latest posts on homepage

--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -2,7 +2,7 @@
 const sequelize = require('sequelize');
 const models = require('../../models');
 
-const articleColors = ["#13005A", "#00337C", "#1C82AD", "#03C988", "#116A7B"];
+const articleColors = ["#E98733", "#CA335C", "#70AFBE", "#7D618F", "#0CA9A8"];
 
 const queryWeeklyTopNews = async () => {
     let news = await models.News.findAll({
@@ -28,11 +28,63 @@ const queryWeeklyTopNews = async () => {
     return news;
 }
 
+const queryTopViewedNews = async () => {
+    let news = await models.News.findAll({
+        attributes: [ 'id', 'title', 'tinyImagePath', 'updatedAt' ],
+        where: { isDraft: false },
+        include: [{
+            model: models.SubCategory,
+            attributes: ['name']
+        }],
+        order: [[ 'totalViewsCount', 'DESC' ]],
+        limit: 10
+    });
+    let colorIndex = 0;
+    news.forEach(element => {
+        let updatedDate = new Date(element.updatedAt);
+        element.updatedAtString = updatedDate.toLocaleString('vi-VN');
+        element.badgeColor = articleColors[colorIndex % articleColors.length];
+        colorIndex++;
+    });
+    return news;
+}
+
+const queryLatestNews = async () => {
+    let news = await models.News.findAll({
+        attributes: [ 'id', 'title', 'tinyImagePath', 'updatedAt' ],
+        where: { isDraft: false },
+        include: [{
+            model: models.SubCategory,
+            attributes: ['name']
+        }],
+        order: [[ 'updatedAt', 'DESC' ]],
+        limit: 10
+    });
+    let colorIndex = 0;
+    news.forEach(element => {
+        let updatedDate = new Date(element.updatedAt);
+        element.updatedAtString = updatedDate.toLocaleString('vi-VN');
+        element.badgeColor = articleColors[colorIndex % articleColors.length];
+        colorIndex++;
+    });
+    return news;
+}
+
 const controller = {};
 
 controller.showHomePage = async (request, response) => {
     const weeklyTopThree = await queryWeeklyTopNews();
     response.locals.weeklyTopThree = weeklyTopThree;
+
+    const topTenMostViews = await queryTopViewedNews();
+    response.locals.twoMostViews = topTenMostViews.slice(0, 2);
+    response.locals.fourMostViews = topTenMostViews.slice(2, 6);
+    response.locals.nextFourMostViews = topTenMostViews.slice(6, 10);
+
+    const topTenLatest = await queryLatestNews();
+    response.locals.twoLatest = topTenLatest.slice(0, 2);
+    response.locals.fourLatest = topTenLatest.slice(2, 6);
+    response.locals.nextFourLatest = topTenLatest.slice(6, 10);
 
     response.render('home');
 };
