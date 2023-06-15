3dec0fd3552d49005dbc4afc6c05f372e6b4f346
tranbaoanhh27 <tranbaoanhh27@gmail.com>
2023-06-15 15:00:50 +0700

    Require user logged in to access page /account

--- routers/user/accountRouter.js
+++ routers/user/accountRouter.js
@@ -2,6 +2,10 @@
 
 const router = require('express').Router();
 const controller = require('../../controllers/user/accountController');
+const authController = require('../../controllers/user/authController');
+
+// Middleware require user logged-in
+router.use(authController.isLoggedIn);
 
 router.get('/', controller.middleware, controller.show);
 router.get('/change-password', controller.middleware, controller.showChangePasswordPage);
