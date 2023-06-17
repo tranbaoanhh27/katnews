348cb67e285715ba3132e24090820b41a4ebc13e
tranbaoanhh27 <tranbaoanhh27@gmail.com>
2023-06-17 16:02:30 +0700

    Completed user's account information page - Update fullname, email, birthdate. - Update avatar, upload avatar image to firebase storage.

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ const { createClient } = require("redis");
 const passport = require("./controllers/passport");
 const connectFlash = require("connect-flash");
 const { createPagination } = require("express-handlebars-paginate");
+const { initializeApp } = require("firebase/app");
+
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+};
+const firebaseApp = initializeApp(firebaseConfig);
+module.exports = firebaseApp;
 
 // Configure redis connection
 const redisClient = createClient({
@@ -89,17 +102,8 @@ app.use("/auth", subdomain("admin", require("./routers/admin/authRouter")));
 app.use(subdomain("admin", require("./routers/admin/adminRouter")));
 
 // User's Middleware
+app.use(require('./controllers/user/accountController').middleware);
 app.use(async (request, response, next) => {
-    // Authentication
-    response.locals.isLoggedIn = request.isAuthenticated();
-
-    if (request.user) {
-        response.locals.headerUser = {
-            fullName: request.user.fullName,
-            isPremium: true,
-        };
-    }
-
     // Query categories for header menu
     const categories = await models.Category.findAll({
         attributes: ["id", "name"],
--- routers/user/accountRouter.js
+++ routers/user/accountRouter.js
@@ -1,13 +1,35 @@
 'use strict';
 
 const router = require('express').Router();
+const multer = require('multer');
 const controller = require('../../controllers/user/accountController');
 const authController = require('../../controllers/user/authController');
+const { body, getErrorMessage } = require('../../controllers/validator');
+
+const multerStorage = multer.memoryStorage();
+const multerUpload = multer({ storage: multerStorage });
 
 // Middleware require user logged-in
 router.use(authController.isLoggedIn);
 
 router.get('/', controller.middleware, controller.show);
+router.post('/',
+    body('email').trim().notEmpty().withMessage('Email không được để trống!').isEmail().withMessage('Email không đúng!'),
+    body('fullName').trim().notEmpty().withMessage('Họ và tên không được để trống!'),
+    (request, response, next) => {
+        const message = getErrorMessage(request);
+        if (message) {
+            response.locals.errorMessage = message;
+            controller.middleware,
+            controller.show
+        }
+        next();
+    },
+    controller.updateAccountInfo,
+    controller.middleware,
+    controller.show
+);
+router.post('/uploadAvatarImage', multerUpload.single('avatar'), controller.updateAvatar);
 router.get('/change-password', controller.middleware, controller.showChangePasswordPage);
 router.get('/premium', controller.middleware, controller.showPremiumPage);
 
