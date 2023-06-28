d3cd768f3f7875cbb6ada0709523b15c5e0d0f31
tranbaoanhh27 <tranbaoanhh27@gmail.com>
2023-06-28 21:59:53 +0700

    Fix bug related to Puppeteer while deploying

--- controllers/user/newsController.js
+++ controllers/user/newsController.js
@@ -279,7 +279,7 @@ controller.postComment = async (request, response) => {
 };
 
 const generatePdf = async (news) => {
-    const browser = await puppeteer.launch({ headless: "new" });
+    const browser = await puppeteer.launch({ headless: "new", args: ['--no-sandbox'] });
     const page = await browser.newPage();
 
     await page.setContent(`
